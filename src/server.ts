e2bf16d225673fb64df45f312c9e868544b298d1
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2024-02-28 22:48:37 -0300

    :rocket:

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server rodando ${env.PORT}`)
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`Server rodando ${env.PORT}`)
+  })
